ffe7084f380be208719eba828d2165c76025a078
vincejordan04-dev <vincejordan04@gmail.com>
2026-01-27 11:25:16 +0800

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+
+}
